8b9d3323809017046ad2c27bd052ce2bf300c824
Celi2812 <celina.shanon@hotmail.com>
2023-02-06 11:21:51 +0100

    new

---
+++ Celina/script.js
@@ -0,0 +1,14 @@
+// Exercise 1:
+
+        let array = [1,7,-3,9];
+        let max = array[0];
+
+        for(let val of array){
+            if (max < val){
+                max = val;
+            }
+        }
+        document.write(max);
+
+
+// Exercise 2:
